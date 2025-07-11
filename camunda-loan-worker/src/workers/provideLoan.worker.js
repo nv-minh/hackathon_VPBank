670ad88b38e0b6ffa93ec913ede5d0374b23cc60
Edward Ngo <minhnv@gtel.vn>
2025-07-11 09:59:37 +0700

    add worker provideLoan

---
+++ camunda-loan-worker/src/workers/provideLoan.worker.js
@@ -0,0 +1,11 @@
+client.subscribe("provideLoan", async ({ task, taskService }) => {
+    console.log(`💸 Nhận được tác vụ [provideLoan]...`);
+
+    const customerData = task.variables.get("customerData");
+    const applicationId = customerData?.application_id || task.variables.get("applicationId");
+
+
+    console.log(`✅ Mô phỏng giải ngân thành công cho đơn vay: ${applicationId}.`);
+
+    await taskService.complete(task);
+});
